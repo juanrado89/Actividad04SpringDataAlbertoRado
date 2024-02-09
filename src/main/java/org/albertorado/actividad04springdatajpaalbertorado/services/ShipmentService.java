88f7373ae5dc2ffc83d53de8edfd2cb945bc45c3
juanrado89 <juan.rado@educa.madrid.org>
2024-02-09 14:28:50 +0100

    se empieza con ShipmentController y ShipmentService

--- src/main/java/org/albertorado/actividad04springdatajpaalbertorado/services/ShipmentService.java
+++ src/main/java/org/albertorado/actividad04springdatajpaalbertorado/services/ShipmentService.java
@@ -1,7 +1,21 @@
 package org.albertorado.actividad04springdatajpaalbertorado.services;
 
+import org.albertorado.actividad04springdatajpaalbertorado.dtos.ShipmentDto;
+import org.albertorado.actividad04springdatajpaalbertorado.repositories.ShipmentRepository;
 import org.springframework.stereotype.Service;
+
+import java.sql.Timestamp;
 
 @Service
 public class ShipmentService {
+
+    private ShipmentRepository shipmentRepository;
+
+    public ShipmentService(ShipmentRepository shipmentRepository) {
+        this.shipmentRepository = shipmentRepository;
+    }
+
+    public ShipmentDto makeShipment(int customerId, Timestamp date, String zipCode, String state, String country, String address, String city){
+
+    }
 }
