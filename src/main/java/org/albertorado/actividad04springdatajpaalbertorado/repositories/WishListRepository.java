557a54503d15e718a0c95ed8ecde78ab828d0410
alberto <juan.rado@educa.madrid.org>
2024-02-12 16:59:33 +0100

    se realizan modificaciones para no usar solo jpql

--- src/main/java/org/albertorado/actividad04springdatajpaalbertorado/repositories/WishListRepository.java
+++ src/main/java/org/albertorado/actividad04springdatajpaalbertorado/repositories/WishListRepository.java
@@ -1,10 +1,8 @@
 package org.albertorado.actividad04springdatajpaalbertorado.repositories;
 
-import jakarta.transaction.Transactional;
 import org.albertorado.actividad04springdatajpaalbertorado.dtos.WishListDto;
 import org.albertorado.actividad04springdatajpaalbertorado.entities.Wishlist;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
