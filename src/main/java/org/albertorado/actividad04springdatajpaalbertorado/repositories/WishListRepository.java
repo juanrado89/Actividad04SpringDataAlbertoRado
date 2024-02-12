63c4a846ec036215fb01fc1385f6e3f1180837e8
alberto <juan.rado@educa.madrid.org>
2024-02-12 16:55:13 +0100

    se realizan modificaciones para no usar solo jpql

--- src/main/java/org/albertorado/actividad04springdatajpaalbertorado/repositories/WishListRepository.java
+++ src/main/java/org/albertorado/actividad04springdatajpaalbertorado/repositories/WishListRepository.java
@@ -9,20 +9,12 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface WishListRepository extends JpaRepository<Wishlist,Integer> {
 
     @Query("select w from Wishlist w WHERE w.customer.customerId = :customer")
     List<WishListDto> getAllByCustomer_CustomerId(@Param("customer") int customerId);
-    List<WishListDto> getByCustomer_CustomerIdAndProduct_ProductId(@Param("customer") int customerId, @Param("product") int productId);
-    @Transactional
-    @Modifying
-    @Query(value = "insert into wishlist (customer_id, product_id) values (:customer, :product)",nativeQuery = true)
-    void insertWishListProduct(@Param("customer") int customerId, @Param("product") int productId);
-    @Transactional
-    @Modifying
-    void removeByCustomer_CustomerIdAndAndProduct_ProductId(@Param("customer") int customerId, @Param("product") int productId);
-    @Transactional
-    @Modifying
-    void removeAllByCustomer_CustomerId(@Param("customer") int customerId);
+    Optional<Wishlist> getByCustomer_CustomerIdAndProduct_ProductId(@Param("customer") int customerId, @Param("product") int productId);
+
 }
--- src/main/java/org/albertorado/actividad04springdatajpaalbertorado/services/WishListService.java
+++ src/main/java/org/albertorado/actividad04springdatajpaalbertorado/services/WishListService.java
@@ -1,36 +1,68 @@
 package org.albertorado.actividad04springdatajpaalbertorado.services;
 
 import org.albertorado.actividad04springdatajpaalbertorado.dtos.WishListDto;
+import org.albertorado.actividad04springdatajpaalbertorado.entities.Customer;
+import org.albertorado.actividad04springdatajpaalbertorado.entities.Product;
+import org.albertorado.actividad04springdatajpaalbertorado.entities.Wishlist;
+import org.albertorado.actividad04springdatajpaalbertorado.repositories.CustomerRepository;
+import org.albertorado.actividad04springdatajpaalbertorado.repositories.ProductRepository;
 import org.albertorado.actividad04springdatajpaalbertorado.repositories.WishListRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class WishListService {
+    private final ProductRepository productRepository;
+    private final CustomerRepository customerRepository;
     private WishListRepository wishListRepository;
 
-    public WishListService(WishListRepository wishListRepository) {
+    public WishListService(WishListRepository wishListRepository, ProductRepository productRepository, CustomerRepository customerRepository) {
         this.wishListRepository = wishListRepository;
+        this.productRepository = productRepository;
+        this.customerRepository = customerRepository;
     }
 
     public List<WishListDto> getAllWishListByCustomerId(int customerId){
         return wishListRepository.getAllByCustomer_CustomerId(customerId);
     }
     public void addProductToWishList(int customerId, int productId){
-        List<WishListDto> producto = wishListRepository.getByCustomer_CustomerIdAndProduct_ProductId(customerId, productId);
-        if(producto.isEmpty()){
-            wishListRepository.insertWishListProduct(customerId, productId);
+        Optional<Wishlist> productList = wishListRepository.getByCustomer_CustomerIdAndProduct_ProductId(customerId, productId);
+        if(productList.isEmpty()){
+            Optional<Product> producto = productRepository.findById(productId);
+            Optional<Customer> cliente = customerRepository.findById(customerId);
+            if(cliente.isPresent()){
+                if(producto.isPresent()){
+                    Wishlist wishList = new Wishlist();
+                    wishList.setProduct(producto.orElseThrow());
+                    wishList.setCustomer(cliente.orElseThrow());
+                    wishListRepository.save(wishList);
+                }
+            }
+
         }
     }
 
     public void removeProduct(int customerId, int productId){
-        wishListRepository.removeByCustomer_CustomerIdAndAndProduct_ProductId(customerId, productId);
+        Optional<Customer> cliente = customerRepository.findById(customerId);
+        Optional<Product> producto = productRepository.findById(productId);
+        if(cliente.isPresent()){
+            if(producto.isPresent()){
+                Optional<Wishlist> eliminar = wishListRepository.getByCustomer_CustomerIdAndProduct_ProductId(customerId, productId);
+                if(eliminar.isPresent()){
+                    wishListRepository.delete(eliminar.orElseThrow());
+                }
+            }
+        }
 
     }
 
     public void removeAllProduct(int customerId) {
-        wishListRepository.removeAllByCustomer_CustomerId(customerId);
-
+        Optional<Customer> cliente = customerRepository.findById(customerId);
+        if(cliente.isPresent()){
+            List<Wishlist> lista = cliente.get().getWishlist();
+            wishListRepository.deleteAllInBatch(lista);
+        }
     }
 }
